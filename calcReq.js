ad80271945305cfe2b1d040a4ffe7035d2c81aee
Marcosdeleprani <marcos_deleprani@estudante.sesisenai.org.br>
2026-03-27 18:07:22 -0300

    calculo req
    atualizei o calculo no req paralelo

--- calcReq.js
+++ calcReq.js
@@ -16,8 +16,10 @@ function reqP(){
     for(let contador = 0; contador < qtd; contador++){
         req[contador] = parseFloat(prompt("digite o valor do resistor numero " + (contador + 1)+":"));
 
-       resultado += req[contador] ** -1;
+       resultado += (1 / req[contador]);
     }
+    resultado = 1 / resultado;
+    
     console.log("o valor final da req em paralelo é: "+ resultado);
     
 }
